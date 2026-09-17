2d91c4693f1246c0ee5637f9791bcbb65687cd01
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 10:07:35 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_1/tutorial_7_srednia_arytmetyczna/script.js
+++ javascript/zmienne i operatory/czesc_1/tutorial_7_srednia_arytmetyczna/script.js
@@ -1,1 +1,5 @@
-// Oblicz średnią z 3 ocen
+const matma = Number(prompt("Podaj ocene z matmy"));
+const polak = Number(prompt("Podaj ocene z polaka"));
+const infa = Number(prompt("Podaj ocene z infy"));
+const srednia = (matma+polak+infa)/3
+document.write(srednia.toFixed(2));
