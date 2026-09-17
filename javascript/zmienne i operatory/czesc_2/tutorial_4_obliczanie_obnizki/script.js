c45034bffc46e92801a71e950baf8c0505f00e03
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 10:25:41 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_2/tutorial_4_obliczanie_obnizki/script.js
+++ javascript/zmienne i operatory/czesc_2/tutorial_4_obliczanie_obnizki/script.js
@@ -1,1 +1,3 @@
-// Pomnóż przez 0.8
+const cenapr = Number(prompt("Ile kosztuje produkt?"));
+const cenapo = cenapr*0.8;
+document.write("Produkt przed obniżką kosztował: "+cenapr+".<br> A po obniżce kosztuje "+cenapo.toFixed(2));
