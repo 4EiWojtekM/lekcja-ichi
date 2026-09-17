f669c9b31789c8e0cd3b62db3547ec9e29ebf8ba
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 09:14:02 +0200

    Update script.js

--- javascript/podstawy/zadanie8_podsumowanie_wizytowka/script.js
+++ javascript/podstawy/zadanie8_podsumowanie_wizytowka/script.js
@@ -1,1 +1,5 @@
-// Twoje rozwiazanie
+//wojtek
+const imie = prompt("Daj mi swoje imie: ");
+const klasa = prompt("W jakiej klasie jesteś: ")
+document.write("Nazywasz się "+imie +" i jesteś w klasie "+klasa);
+console.log(imie +" "+klasa);
