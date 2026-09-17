a67eddffbf1c85d04bc73a37c90154694a66579b
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 09:56:00 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_1/tutorial_5_modulo_reszta/script.js
+++ javascript/zmienne i operatory/czesc_1/tutorial_5_modulo_reszta/script.js
@@ -1,1 +1,5 @@
-// Użyj % do obliczenia reszty
+const cux = prompt("Ile masz cukierków:");
+const bahor = prompt("Ile masz dzieci:");
+const ilosc = Math.floor(cux/bahor);
+const ilosc2 = cux%bahor;
+document.write("Mozesz dac maxymalnie "+ilosc+" cukierków bahorom. A mi zostanie "+ilosc2);
