793c1a39cf3b1921040b801a0f2f7417bb5ae918
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 09:33:03 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_1/tutorial_3_pole_prostokata/script.js
+++ javascript/zmienne i operatory/czesc_1/tutorial_3_pole_prostokata/script.js
@@ -1,1 +1,4 @@
-// Pamiętaj o Number()!
+const bok1 = Number(prompt("Podaj bok a:"));
+const bok2 = Number(prompt("Podaj bok b:"));
+const wynik = bok1 * bok2;
+document.write("Pole twojego prostokąta to: " + wynik);
