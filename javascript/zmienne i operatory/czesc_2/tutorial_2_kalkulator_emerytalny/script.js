b262ebbbfabf89cfc268f54f0d1baa2fa560aa1e
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 10:17:36 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_2/tutorial_2_kalkulator_emerytalny/script.js
+++ javascript/zmienne i operatory/czesc_2/tutorial_2_kalkulator_emerytalny/script.js
@@ -1,1 +1,5 @@
-// Stała WIEK_EMERYTALNY = 65
+const wieke = 65;
+const imie = prompt("Podaj imie");
+const wiek = Number(prompt("Podaj wiek"));
+const eme = wieke - wiek;
+document.write("Cześć "+imie+" do emerytury pozostało ci "+eme+" lat");
