bd158cebdb7c249db14ae99a883e3f27e079c527
Ninek <asfaltstrasse7@gmail.com>
2026-09-17 10:13:30 +0200

    Update script.js

--- javascript/zmienne i operatory/czesc_2/tutorial_1_formatowanie_adresu/script.js
+++ javascript/zmienne i operatory/czesc_2/tutorial_1_formatowanie_adresu/script.js
@@ -1,1 +1,7 @@
-// Pobierz dane i wyświetl adres
+const miasto = prompt("podaj miasto:");
+const nrpo = prompt("podaj numer pocztowy:");
+const ulica = prompt("podaj ulice:");
+const nrdo = prompt("podaj nr domu:");
+document.write(ulica +" "+nrdo);
+document.write("<br>");
+document.write(miasto+" "+nrpo);
